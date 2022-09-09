b50f46b706e787c1b612491675f018dd2fe4ec15
Samuel Hincapie Montoya (24191) <samuel.hincapie@est.zamorano.edu>
2022-09-09 17:21:13 -0600

    Versión casi lista

---
+++ lab02/celcius2farenheit.py
@@ -0,0 +1,6 @@
+#Tendencias e Innovacion en Tecnologia Agricola
+tempCelcius = float(input("Cual es la temperatura en Celcius? "))
+tempFarenheit = (tempCelcius * 1.8) + 32
+
+print("Temperatura en Farenheit = " + str(tempFarenheit))
+
--- lab02/imprimirnombre.py
+++ lab02/imprimirnombre.py
@@ -1,3 +1,3 @@
 #Tendencias e Innovacion en Tecnologia Agricola
-nombre = input("cual es su nombre?")
-print("Hola" + nombre)
+nombre = input("cual es su nombre? ")
+print("Hola " + nombre)
